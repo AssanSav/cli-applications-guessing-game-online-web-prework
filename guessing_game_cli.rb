733e2447bf3491912f34a51cb17f51d81d8ece9f
assane savadogo <assane.savadogo81@gmail.com>
2019-07-09 20:43:00 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,5 +2,7 @@ def run_guessing_game
   number = rand(6) + 1
   user_input = gets.chomp
   if user_input == number.to_i
-    
+    puts "You guessed the correct number!"
+  elsif user_input != number.to_i
+    puts 
 end
